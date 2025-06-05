4b19db04dc610b1ff5ac2bff36a7a083061ca58a
harimmon <bhr0502@gmail.com>
2025-06-05 17:16:18 +0900

    login 완료

--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -13,6 +13,27 @@ class UserRepository {
     Response response = await dio.post("/join", data: requestBody);
     final responseBody = response.data;
     Logger().d(responseBody);
-    return requestBody;
+    return responseBody;
+  }
+
+  Future<Map<String, dynamic>> login(String username, String password) async {
+    // 1. Map 변환
+    final requestBody = {
+      "username": username,
+      "password": password,
+    };
+
+    // 2. 통신
+    Response response = await dio.post("/login", data: requestBody);
+    Map<String, dynamic> responseBody = response.data;
+
+    // 3. 헤더에서 토큰을 바디로 옮기기
+    String accessToken = "";
+    try {
+      accessToken = response.headers["Authorization"]![0];
+      responseBody["response"]["accessToken"] = accessToken;
+    } catch (e) {}
+    Logger().d(responseBody);
+    return responseBody;
   }
 }
