a653ce2aa591a43a05dbdb29c9993e8378005929
harimmon <bhr0502@gmail.com>
2025-06-09 15:00:37 +0900

    post list

---
+++ lib/ui/pages/post/list_page/post_list_vm.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_blog/data/model/post.dart';
+import 'package:flutter_blog/data/repository/post_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// 1. 창고 관리자
+final postListProvider = NotifierProvider<PostListVM, PostListModel?>(() {
+  return PostListVM();
+});
+
+// 2. 창고
+class PostListVM extends Notifier<PostListModel?> {
+  @override
+  PostListModel? build() {
+    init();
+    return null;
+  }
+
+  Future<void> init({int page = 0}) async {
+    Map<String, dynamic> body = await PostRepository().getList(page: page);
+    state = PostListModel.fromMap(body["response"]);
+  }
+}
+
+// 3. 창고 데이터 타입
+class PostListModel {
+  bool isFirst;
+  bool isLast;
+  int pageNumber;
+  int size;
+  int totalPage;
+  List<Post> posts;
+
+  PostListModel(this.isFirst, this.isLast, this.pageNumber, this.size, this.totalPage, this.posts);
+
+  PostListModel.fromMap(Map<String, dynamic> data)
+      : isFirst = data['isFirst'],
+        isLast = data['isLast'],
+        pageNumber = data['pageNumber'],
+        size = data['size'],
+        totalPage = data['totalPage'],
+        posts = (data['posts'] as List).map((e) => Post.fromMap(e)).toList();
+
+  PostListModel copyWith({
+    bool? isFirst,
+    bool? isLast,
+    int? pageNumber,
+    int? size,
+    int? totalPage,
+    List<Post>? posts,
+  }) {
+    return PostListModel(isFirst ?? this.isFirst, isLast ?? this.isLast, pageNumber ?? this.pageNumber, size ?? this.size,
+        totalPage ?? this.totalPage, posts ?? this.posts);
+  }
+
+  @override
+  String toString() {
+    return 'PostListModel{isFirst: $isFirst, isLast: $isLast, pageNumber: $pageNumber, size: $size, totalPage: $totalPage, posts: $posts}';
+  }
+}
