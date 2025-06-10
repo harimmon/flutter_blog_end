ad7f957716f0bfb6dca6b431fec0887aeb207dd9
harimmon <bhr0502@gmail.com>
2025-06-10 11:11:51 +0900

    write 2 완료

--- lib/ui/pages/post/write_page/widgets/post_write_form.dart
+++ lib/ui/pages/post/write_page/widgets/post_write_form.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/_core/constants/size.dart';
 import 'package:flutter_blog/ui/pages/post/list_page/post_list_vm.dart';
-import 'package:flutter_blog/ui/pages/post/write_page/post_write_fm.dart';
 import 'package:flutter_blog/ui/widgets/custom_elavated_button.dart';
 import 'package:flutter_blog/ui/widgets/custom_text_area.dart';
 import 'package:flutter_blog/ui/widgets/custom_text_form_field.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PostWriteForm extends ConsumerWidget {
+  late String title;
+  late String content;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    PostWriteFM fm = ref.read(postWriteProvider.notifier);
     PostListVM vm = ref.read(postListProvider.notifier);
-    PostWriteModel model = ref.watch(postWriteProvider);
+
     return Form(
       child: ListView(
         shrinkWrap: true,
@@ -20,21 +21,21 @@ class PostWriteForm extends ConsumerWidget {
           CustomTextFormField(
             hint: "Title",
             onChanged: (value) {
-              fm.title(value);
+              title = value;
             },
           ),
           const SizedBox(height: smallGap),
           CustomTextArea(
             hint: "Content",
             onChanged: (value) {
-              fm.content(value);
+              content = value;
             },
           ),
           const SizedBox(height: largeGap),
           CustomElevatedButton(
             text: "글쓰기",
             click: () {
-              vm.write(model.title, model.content);
+              vm.write(title, content);
             },
           ),
         ],
